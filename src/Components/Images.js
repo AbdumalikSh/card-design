9fb3971596b8b3681af14e7ab1c091c2241038dc
AbdumalikSh <shamf120804@gmail.com>
2024-03-15 21:10:37 +0300

    ...1

---
+++ src/Components/Images.js
@@ -0,0 +1,13 @@
+import img1 from './Images/1.png';
+import img2 from './Images/2.png';
+import img3 from './Images/3.png';
+import img4 from './Images/4.png';
+import img5 from './Images/5.png';
+import img6 from './Images/6.png';
+import img7 from './Images/7.png';
+import img8 from './Images/8.png';
+import img9 from './Images/9.png';
+
+const imgSrc = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
+
+export default imgSrc;
--- src/Components/ImgGallery.jsx
+++ src/Components/ImgGallery.jsx
@@ -1,29 +1,16 @@
 import React from 'react';
 import Img from './Img';
 import styles from './imgGallery.module.css'
-import img1 from './Images/1.png'
-import img2 from './Images/2.png'
-import img3 from './Images/3.png'
-import img4 from './Images/4.png'
-import img5 from './Images/5.png'
-import img6 from './Images/6.png'
-import img7 from './Images/7.png'
-import img8 from './Images/8.png'
-import img9 from './Images/9.png'
+import ImgSrc from './Images'
 
 function ImgGallery (props) {
+    const images = ImgSrc.map((img, index) => {
+        return <Img key = {index} src = {img} onClick = {props.onClick}/>
+    })
     return (
         <>
             <div className={styles.imgs}>
-                <Img src = {img1} onClick={props.onClick}/>
-                <Img src = {img2} onClick={props.onClick}/>
-                <Img src = {img3} onClick={props.onClick}/>
-                <Img src = {img4} onClick={props.onClick}/>
-                <Img src = {img5} onClick={props.onClick}/>
-                <Img src = {img6} onClick={props.onClick}/>
-                <Img src = {img7} onClick={props.onClick}/>
-                <Img src = {img8} onClick={props.onClick}/>
-                <Img src = {img9} onClick={props.onClick}/> 
+                {images}
             </div>
         </>
     )
